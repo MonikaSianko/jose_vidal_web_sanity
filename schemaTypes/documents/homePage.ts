d7ac25189ef0f29f8571f80b329c0769a2ea91e6
MonikaSianko <monika.sianko225@gmail.com>
2026-01-06 17:14:10 +0100

    delete hero bg photo

--- schemaTypes/documents/homePage.ts
+++ schemaTypes/documents/homePage.ts
@@ -1,37 +1,74 @@
-import { defineType, defineField } from 'sanity'
-import { languages } from '../../languages'
+import { defineType, defineField } from "sanity";
+import { languages } from "../../languages";
 
 export default defineType({
-  name: 'homePage',
-  title: 'Home Page',
-  type: 'document',
+  name: "homePage",
+  title: "Home Page",
+  type: "document",
   fields: [
     defineField({
-      name: 'language',
-      title: 'Language',
-      type: 'string',
+      name: "language",
+      title: "Language",
+      type: "string",
       options: {
         list: languages.map((lang) => ({ title: lang.title, value: lang.id })),
       },
       validation: (Rule) => Rule.required(),
     }),
 
-    defineField({ name: 'hero', title: 'Hero', type: 'homeHero' }),
-    defineField({ name: 'about', title: 'About', type: 'homeAbout' }),
     defineField({
-      name: 'testimonials',
-      title: 'Testimonials',
-      type: 'homeTestimonials',
+      name: "hero",
+      title: "Hero Section",
+      type: "homeHero",
+      options: { collapsible: true, collapsed: true },
     }),
-    defineField({ name: 'author', title: 'Author', type: 'homeAuthor' }),
-    defineField({ name: 'courses', title: 'Courses', type: 'homeCourses' }),
-    defineField({ name: 'cta', title: 'CTA', type: 'homeCTA' }),
-    defineField({ name: 'contact', title: 'Contact', type: 'homeContact' }),
+
+    defineField({
+      name: "about",
+      title: "About Section",
+      type: "homeAbout",
+      options: { collapsible: true, collapsed: true },
+    }),
+
+    defineField({
+      name: "testimonials",
+      title: "Testimonials Section",
+      type: "homeTestimonials",
+      options: { collapsible: true, collapsed: true },
+    }),
+
+    defineField({
+      name: "author",
+      title: "Author Section",
+      type: "homeAuthor",
+      options: { collapsible: true, collapsed: true },
+    }),
+
+    defineField({
+      name: "courses",
+      title: "Courses Section",
+      type: "homeCourses",
+      options: { collapsible: true, collapsed: true },
+    }),
+
+    defineField({
+      name: "cta",
+      title: "CTA Section",
+      type: "homeCTA",
+      options: { collapsible: true, collapsed: true },
+    }),
+
+    defineField({
+      name: "contact",
+      title: "Contact Section",
+      type: "homeContact",
+      options: { collapsible: true, collapsed: true },
+    }),
   ],
   preview: {
-    select: { title: 'language' },
+    select: { title: "language" },
     prepare({ title }) {
-      return { title: `Home Page (${title ?? 'no language'})` }
+      return { title: `Home Page (${title ?? "no language"})` };
     },
   },
-})
+});
