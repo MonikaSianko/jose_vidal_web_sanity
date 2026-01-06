e3d5514a8a0bd382f1c2820142dcdcbabe8d5c93
MonikaSianko <monika.sianko225@gmail.com>
2026-01-06 15:37:01 +0100

    delete line1 and 2 from address

--- schemaTypes/objects/address.ts
+++ schemaTypes/objects/address.ts
@@ -1,14 +1,13 @@
-import { defineField, defineType } from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'address',
-  title: 'Address',
-  type: 'object',
+  name: "address",
+  title: "Address",
+  type: "object",
   fields: [
-    defineField({ name: 'line1', title: 'line1', type: 'string' }),
-    defineField({ name: 'line2', title: 'line2', type: 'string' }),
-    defineField({ name: 'city', title: 'city', type: 'string' }),
-    defineField({ name: 'postalCode', title: 'postalCode', type: 'string' }),
-    defineField({ name: 'country', title: 'country', type: 'string' }),
+    defineField({ name: "street", title: "street and number", type: "string" }),
+    defineField({ name: "city", title: "city", type: "string" }),
+    defineField({ name: "postalCode", title: "postalCode", type: "string" }),
+    defineField({ name: "country", title: "country", type: "string" }),
   ],
-})
+});
